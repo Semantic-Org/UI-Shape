30c91dc08ed250b74241c0b31eee108f62ebba17
Jack Lukic <jack@semantic-ui.com>
2015-02-18 14:14:45 -0500

    Updated component to version 1.9.0

---
+++ package.js
@@ -0,0 +1,17 @@
+var
+  where = 'client' // Adds files only to the client
+;
+
+Package.describe({
+  name    : 'semantic:ui-shape',
+  summary : 'Semantic UI - Shape (official): Single component release of shape',
+  version : '1.9.0',
+  git     : 'git://github.com/Semantic-Org/UI-Shape.git',
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0');
+  api.addFiles([
+    
+  ], where);
+});
