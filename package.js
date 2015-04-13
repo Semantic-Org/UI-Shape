de052a6ddb7d3ed84204cac54ea75e2665b61bfe
Pusher Robot <jack@semantic-ui.com>
2015-04-13 15:24:03 -0400

    Updated component to version 1.11.7

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-shape',
   summary : 'Semantic UI - Shape: Single component release',
-  version : '1.11.6',
+  version : '1.11.7',
   git     : 'git://github.com/Semantic-Org/UI-Shape.git',
 });
 
