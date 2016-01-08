2bd8dc38d69703588ed82853d123896e8c83a094
Jack Lukic <jack@semantic-ui.com>
2016-01-07 23:55:08 -0500

    Updated component to version 2.1.8

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-shape',
   summary : 'Semantic UI - Shape: Single component release',
-  version : '2.1.7',
+  version : '2.1.8',
   git     : 'git://github.com/Semantic-Org/UI-Shape.git',
 });
 
